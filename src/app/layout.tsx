cc7f1b21741bd919f6de059a172f62f2894b49cb
Harsh Bhojwani <rebootcode2024@gmail.com>
2024-08-13 09:51:48 +0530

    metadata corrected

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,12 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Aquaregia",
-  description: "Aquaregia - Echo of Liberation",
+  title: "Aquaregia", // This is the title of the page
+  description: "Echo of Liberation", // This is the description
+  openGraph: {
+    title: "Aquaregia", // This is the title that will appear on social media shares
+    siteName: "Aquaregia - Echo of Liberation", // This is the name of the site that will appear
+  },
 };
 
 export default function RootLayout({
@@ -17,11 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <meta name="description" content="Aquaregia - Echo of Liberation" />
+        {/* Add the link to the favicon */}
         <link rel="icon" href="/assets/logo-tpt.png" sizes="64x64" />
-        <title>Aquaregia</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Aquaregia</title> {/* Title tag in case you want to override */}
       </head>
       <body className={inter.className}>{children}</body>
     </html>
